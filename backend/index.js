03e383358207bd205b33898fdab6a858c2e99c9f
miatia1025 <miatia1025@gmail.com>
2024-09-05 17:32:39 +0900

    update: backend done

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,15 @@
 import express from "express";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
 
-//
 import authRoutes from "./routes/authRoutes.js";
 
 const port = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello, World");
-});
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,9 +6,13 @@ import {
   verifyEmail,
   forgotPassword,
   resetPassword,
+  checkAuth,
 } from "../controllers/authController.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signup);
 router.post("/login", login);
