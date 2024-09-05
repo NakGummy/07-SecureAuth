23c6e495ef4153ceeee1c312c58210b5e5752210
miatia1025 <miatia1025@gmail.com>
2024-09-05 18:57:20 +0900

    update: frontend signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import Input from "../components/Input";
+import { User, Mail, Lock } from "lucide-react";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          Create Account
+        </h2>
+
+        <form onSubmit={handleSignUp}>
+          {/* Username */}
+          <Input
+            icon={User}
+            type="text"
+            placeholder="Full Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {/* Email */}
+          <Input
+            icon={Mail}
+            type="text"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {/* Password */}
+          <Input
+            icon={Lock}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </form>
+      </div>
+    </motion.div>
+  );
 };
 
 export default SignUpPage;
